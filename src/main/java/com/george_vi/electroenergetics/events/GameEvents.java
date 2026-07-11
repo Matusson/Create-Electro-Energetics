1f62c613220a85754a0fffadfb3844950edec117
george8188625 <135385317+george8188625@users.noreply.github.com>
2026-07-11 12:59:31 +0200

    placeable in electrical panel tooltip

--- src/main/java/com/george_vi/electroenergetics/events/GameEvents.java
+++ src/main/java/com/george_vi/electroenergetics/events/GameEvents.java
@@ -6,6 +6,7 @@ import com.george_vi.electroenergetics.client.ElectricPropertiesOverlay;
 import com.george_vi.electroenergetics.client.WireEffects;
 import com.george_vi.electroenergetics.client.WireRenderer;
 import com.george_vi.electroenergetics.commands.CEECommands;
+import com.george_vi.electroenergetics.config.CEEConfigs;
 import com.george_vi.electroenergetics.content.accumulator.AccumulatorBlock;
 import com.george_vi.electroenergetics.content.bulb.BulbDevice;
 import com.george_vi.electroenergetics.content.converter.ConverterBlockEntity;
@@ -27,6 +28,7 @@ import com.george_vi.electroenergetics.content.wire_spool.WireApplyingBehaviour;
 import com.george_vi.electroenergetics.content.wire_spool.WireSparkEffectTicker;
 import com.george_vi.electroenergetics.devices.device.DevicesSavedData;
 import com.george_vi.electroenergetics.foundation.CEEHoldInteractionHandler;
+import com.george_vi.electroenergetics.foundation.CEELang;
 import com.george_vi.electroenergetics.foundation.nodes.InWorldNode;
 import com.george_vi.electroenergetics.simulation.infrastructure.InWorldNodeData;
 import com.george_vi.electroenergetics.simulation.infrastructure.InfrastructureSavedData;
@@ -60,6 +62,7 @@ import net.neoforged.neoforge.client.event.RenderHighlightEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.tick.EntityTickEvent;
@@ -350,6 +353,15 @@ public class GameEvents {
     @SubscribeEvent
     public static void reloadLevelRenderer(ReloadLevelRendererEvent event) {
         WireRenderer.recreateVisuals();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void itemTooltip(ItemTooltipEvent event) {
+        if (event.getItemStack().is(CEETags.ELECTRICAL_PANEL_ATTACHMENT) &&
+                CEEConfigs.client().displayPanelTooltip.get()) {
+            event.getToolTip().add(CEELang.translateDirect("hint.placeable_in_electrical_panel"));
+        }
     }
 
 }
